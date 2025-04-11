991cd09819d99ea4f185826d50fc298e0d915960
coeusyk <69001824+coeusyk@users.noreply.github.com>
2025-04-11 16:25:09 +0530

    Resolved path errors

--- trovio/src/components/recommendation-card.tsx
+++ trovio/src/components/recommendation-card.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/src/components/ui/card";
 import { MapPin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
